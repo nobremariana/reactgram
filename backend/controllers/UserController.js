4d604e3009ae78ae7cff83df4264385a1c61c26e
Mariana Nobre <nobremariana9@gmail.com>
2026-01-26 14:28:35 -0300

    feat: add login validation and authentication

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -12,8 +12,62 @@ const generateToken = (id) => {
 
 // User Registration
 const register = async (req, res) => {
-    res.send('User registration endpoint');
+  const { name, email, password } = req.body;
+
+  // check if user exists
+  const user = await User.findOne({ email });
+
+  if (user) {
+    res.status(422).json({ errors: ["Por favor, utilize outro e-mail."] });
+    return;
+  }
+  // create password hash
+  const salt = await bcrypt.genSalt();
+  const passwordHash = await bcrypt.hash(password, salt);
+
+  // create user
+  const newUser = await User.create({
+    name,
+    email,
+    password: passwordHash,
+  })
+
+  // if user was created successfully, return token
+  if (!newUser) {
+    res.status(422).json({ errors: ["Houve um erro, por favor tente novamente mais tarde."] });
+    return;
+  }
+  res.status(201).json({
+    _id: newUser._id,
+    token: generateToken(newUser._id),
+  });
 };
+// sing user in
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  // check if user exists
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    res.status(404).json({ errors: ["Usuário não encontrado."] });
+    return;
+  }
+
+  // check if password matches
+  if (!(await bcrypt.compare(password, user.password))) {
+    res.status(422).json({ errors: ["Senha inválida."] });
+    return;
+  }
+  // return user with token
+  res.status(200).json({
+    _id: user._id,
+    profileImage: user.profileImage,
+    token: generateToken(user._id),
+  });
+}
+
 module.exports = {
   register,
+  login
 };
--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -2,13 +2,14 @@ const express = require("express");
 const router = express.Router();
 
 //controller
-const { register } = require("../controllers/UserController");
+const { register, login } = require("../controllers/UserController");
 
 //middlewares
 const validate = require("../middlewares/handleValidations");
-const {userCreateValidation} = require("../middlewares/userValidation");
+const {userCreateValidation, loginValidation} = require("../middlewares/userValidation");
 
 //routes
 router.post("/register", userCreateValidation(), validate, register);
+router.post("/login", loginValidation(), validate, login);
 
 module.exports = router; 
