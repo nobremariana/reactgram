ed2d45b168ce7dfc695050bb1034308c33b9557d
Mariana Nobre <nobremariana9@gmail.com>
2026-01-26 10:56:39 -0300

    chore: configure database connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,1 +1,20 @@
 const mongoose = require("mongoose");
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
+
+const conn = async () => {
+  try {
+    const dbConn = await mongoose.connect(
+      `mongodb+srv://${dbUser}:${dbPassword}@cluster0.e2vthin.mongodb.net/?appName=Cluster0`
+    );
+    console.log("Conectou ao banco de dados!");
+
+    return dbConn;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+conn();
+
+module.exports = conn;
