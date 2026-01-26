5ebef393dbef13340fa58d4033743c21de6881c5
Mariana Nobre <nobremariana9@gmail.com>
2026-01-26 13:39:07 -0300

    feat: add user creation validation

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -4,7 +4,11 @@ const router = express.Router();
 //controller
 const { register } = require("../controllers/UserController");
 
+//middlewares
+const validate = require("../middlewares/handleValidations");
+const {userCreateValidation} = require("../middlewares/userValidation");
+
 //routes
-router.post("/register", register);
+router.post("/register", userCreateValidation(), validate, register);
 
 module.exports = router; 
