d998d0d26f33a5cb56efde099e29114e36dccfa4
Mariana Nobre <nobremariana9@gmail.com>
2026-01-27 10:49:42 -0300

    feat: remove photo

--- backend/controllers/PhotoController.js
+++ backend/controllers/PhotoController.js
@@ -28,6 +28,42 @@ const insertPhoto = async (req, res) => {
 
 };
 
+// remove a photo from the DB
+const deletePhoto = async (req, res) => {
+  const { id } = req.params;
+
+  const reqUser = req.user;
+  
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(422).json({
+      errors: ["ID inválido."],
+    });
+  }
+
+  const photo = await Photo.findById(id);
+  
+  // Check if photo exists
+  if (!photo) {
+    res.status(404).json({ errors: ["Foto não encontrada!"] });
+    return;
+  }
+
+  // Check if photo belongs to user
+  if (!photo.userId.equals(reqUser._id)) {
+    res
+      .status(422)
+      .json({ errors: ["Ocorreu um erro, tente novamente mais tarde"] });
+    return;
+  }
+
+  await Photo.findByIdAndDelete(photo._id);
+
+  res
+    .status(200)
+    .json({ id: photo._id, message: "Foto excluída com sucesso." });
+};
+
 module.exports = {
   insertPhoto,
+  deletePhoto,
 };
