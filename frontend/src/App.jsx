26c83f89c2146dc253926b26a2b5a922a24adca6
Mariana Nobre <nobremariana9@gmail.com>
2026-01-28 10:10:40 -0300

    chore: configure React Router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,27 @@
 import { useState } from 'react'
 import './App.css'
 
+// router
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+// pages
+import Home from './pages/Home/Home';
+import Login from './pages/Auth/Login';
+import Register from './pages/Auth/Register';
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <h1>ReactGram</h1>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </BrowserRouter>
       </div>
     </>
   )
