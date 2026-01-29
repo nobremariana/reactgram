a7cbe9345c5a1b7c82a30341a8d81f9e8b8029c4
Mariana Nobre <nobremariana9@gmail.com>
2026-01-29 12:15:13 -0300

    feat(user): add profile update with image upload and success feedback

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Solve CORS
-app.use(cors({ credentials: true, origin: "http://localhost:5174" }));
+app.use(cors());
 
 // Upload directory
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,10 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";
-// import userReducer from "./slices/userSlice";
+import userReducer from "./slices/useSlice";
 // import photoReducer from "./slices/photoSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    user: userReducer,
   },
 });
